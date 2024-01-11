b8d251ec73192eeb6c675e61dfbd0615c16a34b2
Kast <144546110+Kast0000@users.noreply.github.com>
2024-01-11 20:45:15 +0100

    Update and rename README.md to README.md.lua

---
+++ README.md.lua
@@ -0,0 +1,10 @@
+--[[
+	Osiris.cc Remake Leaked By Kast
+   Script Description
+   This is not real osiris.cc this is a remake that uses the same source as them [ ALMOST ]
+   I would recommend using this because 60$ for a script thats barley good is insane
+   Good features auto resolver that hits shots and resolves good auto prediction and good gunfov
+   Some of the features are fake like AirShotSmooth and stickness [ mostly features that isnt being used alot ]
+   Some features arent included in osiris so i decided to add some of custom features that would make osiris better
+   Its also really buggy so yea enjoy
+--]]
